00271a49b775551f4896f9f898fd74fd4ce55cd8
skrijwan <skrijwan329@gmail.com>
2024-10-25 22:50:31 +0530

    new update

--- src/component/Addproduct.js
+++ src/component/Addproduct.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Addproduct({showAlert}) {
+
     useEffect(()=>{
         const token= localStorage.getItem("admin-token")
         if(!token){
